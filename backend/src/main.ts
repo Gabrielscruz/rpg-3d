ff04ee0e921fa45a0baef159d09a4136816ebcc7
Gabriel Silva <gabriel-blz@outlook.com.br>
2026-04-17 21:27:24 -0300

    feat: nest cli json

---
+++ backend/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  // Inicialização do Nest utilizando o motor do Fastify
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  
+  // Habilita Cors para que o Front (Vite porta 3000) possa conectar na API porta 3001
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+  
+  await app.listen(3001, '0.0.0.0');
+}
+bootstrap();
