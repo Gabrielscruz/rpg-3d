90b8a9b9d0b587dc06be0098b35025fef85fc390
Gabriel Silva <gabriel-blz@outlook.com.br>
2026-08-20 23:24:04 -0300

    chore: remove backend and add vercel config

--- backend/src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class AppController {
-  @Get()
-  getHello() {
-    return { status: 'ok', message: 'VTT Backend is running on Fastify!' };
-  }
-}
